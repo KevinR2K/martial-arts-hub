e988135f6a4890baaa0d9fad8ca722325ae5cdc8
KEVIN RASHEL <kevinrashel20060209@gmail.com>
2026-08-07 00:27:41 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Martial Arts Hub</title>
+     <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <header class="header">
+        <nav class="navbar">
+            <h1 class="logo">Martial Arts Hub</h1>
+            <ul class="nav-links">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Articales</a></li>
+                <li><a href="#">Fighters</a></li>
+                <li><a href="#">Categories</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Login</a></li>
+            </ul>
+            
+              <div class="auth-buttons">
+            <a href="login.php" class="login-btn">Login</a>
+            <a href="signup.php" class="signup-btn">Sign Up</a>
+        </div>
+
+        </nav>
+    </header>
+    
+</body>
+</html>
